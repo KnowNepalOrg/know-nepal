300b35f7193a08d079508d71229f23955152b6ea
Suckscorpio999 <olinabin214@gmail.com>
2026-07-17 21:59:50 +0545

    build: add application entry point

---
+++ know-nepal-geography/src/main/java/com/knownepal/geography/KnowNepalGeographyApplication.java
@@ -0,0 +1,13 @@
+package com.knownepal.geography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowNepalGeographyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowNepalGeographyApplication.class, args);
+	}
+
+}
